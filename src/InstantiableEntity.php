da162513f8b75a610e027553b5c9097184544e54
Stephen Beemsterboer <balbuf@gmail.com>
2016-06-01 15:18:28 -0400

    Fix instantiation with name

--- src/InstantiableEntity.php
+++ src/InstantiableEntity.php
@@ -8,8 +8,10 @@ namespace OomphInc\Bases;
 
 class InstantiableEntity extends BaseEntity {
 
-	function __construct( $name, array $properties = [] ) {
-		$this->name = $name;
+	function __construct( $name = null, array $properties = [] ) {
+		if ( isset( $name ) ) {
+			$this->name = $name;
+		}
 		// for convenience, properties can be passed as an array upon instantiation
 		foreach ( $properties as $name => $value ) {
 			$this->$name = $value;
